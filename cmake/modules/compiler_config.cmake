40de9b8ba2a742d6616eeb623d843f173a3e5df8
Southern, Gabriel <gabriel.southern@intel.com>
2017-12-05 05:30:35 -0800

    Initial support for sanitizers

--- cmake/modules/compiler_config.cmake
+++ cmake/modules/compiler_config.cmake
@@ -93,6 +93,25 @@ if(CMAKE_COMPILER_IS_GNUCC)
   endif()
 endif(CMAKE_COMPILER_IS_GNUCC)
 
+if(USE_SANITIZER)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-omit-frame-pointer")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
+    if(USE_SANITIZER STREQUAL "Address")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
+    elseif(USE_SANITIZER STREQUAL "Thread")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=thread")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread")
+    elseif(USE_SANITIZER STREQUAL "Memory")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=memory")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=memory")
+    elseif(USE_SANITIZER STREQUAL "Undefined")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=undefined")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
+    endif()
+endif()
+
+
 ############################################################################
 ## Defensive compilation for Release #######################################
 #######################################################################
